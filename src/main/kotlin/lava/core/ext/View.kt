3152b1cc742931560a1f8c61cbb68ff695d0e7c6
svc <wuxi@flatincbr.com>
2021-01-06 20:42:05 +0800

    single safe click

--- src/main/kotlin/lava/core/ext/View.kt
+++ src/main/kotlin/lava/core/ext/View.kt
@@ -2,6 +2,29 @@ package lava.core.ext
 
 import android.view.LayoutInflater
 import android.view.View
+import com.example.lava.R
 
 val View.inflater: LayoutInflater
     get() = LayoutInflater.from(context)
+
+fun View?.singleClick(interval: Long = 500, block: (v: View) -> Unit) {
+    this?.setOnClickListener {
+        val lastTime = getTag(R.id.last_click_time) as? Long ?: 0L
+        if (lastTime - interval >= System.currentTimeMillis()) {
+            kotlin.runCatching {
+                block(this)
+            }.onFailure {
+                it.printStackTrace()
+            }
+            setTag(R.id.last_click_time, System.currentTimeMillis())
+        }
+    }
+}
+
+fun View?.safeClick(block: (v: View) -> Unit){
+    this?.setOnClickListener {
+        kotlin.runCatching {
+            block(this)
+        }
+    }
+}
