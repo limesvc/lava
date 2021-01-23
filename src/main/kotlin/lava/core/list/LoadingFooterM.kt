e6bfb63a9e0c803b191f63427d7fe1cb4360fb71
svc <13126065403@wx>
2021-01-23 09:44:00 +0800

    补完footer

--- src/main/kotlin/lava/core/list/LoadingFooterM.kt
+++ src/main/kotlin/lava/core/list/LoadingFooterM.kt
@@ -1,8 +1,11 @@
 package lava.core.list
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
+import android.view.Gravity
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import lava.core.util.LPUtil
 import lava.core.util.MATCH
@@ -10,11 +13,15 @@ import lava.core.util.MATCH
 class LoadingFooterM @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LoadingFooterX(context, attrs, defStyleAttr) {
+    private val imageView = ImageView(context)
+    private val textView = TextView(context)
+
     init {
         val progressDrawable = CircularProgressDrawable(context)
-        val imageView = ImageView(context)
         imageView.setImageDrawable(progressDrawable)
         addView(imageView, LPUtil.viewGroup(MATCH, 36))
+        textView.gravity = Gravity.CENTER
+        textView.setTextColor(Color.BLACK)
 
         setOnClickListener {
             if (state == LoadMoreState.ERROR) {
@@ -25,7 +32,14 @@ class LoadingFooterM @JvmOverloads constructor(
 
     override fun asView() = this
 
-    override fun onError() {
+    override fun onLoading() {
+        super.onLoading()
+        removeAllViews()
+        addView(imageView, LPUtil.viewGroup())
+    }
 
+    override fun onError() {
+        removeAllViews()
+        addView(textView, LPUtil.viewGroup())
     }
 }
