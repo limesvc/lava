e16d150ed43cd53669dfad6ee35f222416078a86
svc <13126065403@wx>
2021-02-21 12:10:46 +0800

    stick 1.0

--- src/main/kotlin/lava/core/widget/StickHeadViewX.kt
+++ src/main/kotlin/lava/core/widget/StickHeadViewX.kt
@@ -8,6 +8,8 @@ import androidx.annotation.IdRes
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import lava.core.ext.am
+import lava.core.ext.onNotNull
+import lava.core.ext.removeFromParent
 import lava.core.ext.replaceParent
 
 /**
@@ -16,47 +18,105 @@ import lava.core.ext.replaceParent
 class StickHeadViewX @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
-    private var viewTypeMap = mutableMapOf<Int, Int>()
-    private var holderMap = mutableMapOf<Int, RecyclerView.ViewHolder>()
+    private lateinit var recyclerView: RecyclerView
+
+    private val stickerMap = mutableMapOf<Int, Sticker>()
+
+    private var curSticker: Sticker? = null
 
     fun attach(recyclerView: RecyclerView, @IdRes vararg headerId: Int) {
+        this.recyclerView = recyclerView
         headerId.forEach {
-            viewTypeMap[it] = -1
+            stickerMap[it] = Sticker(-1, null, -1)
         }
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                // 减少一层嵌套
+                // 减少一层格式嵌套
                 onScrolled(recyclerView, this)
             }
         })
     }
 
     private fun onScrolled(recyclerView: RecyclerView, listener: RecyclerView.OnScrollListener) {
-        val firstView = recyclerView.firstVisibleItem() ?: return
-        val view = recyclerView.firstCompletelyVisibleItem() ?: return
+        val first = recyclerView.firstVisibleItem() ?: return
+        val next = recyclerView.firstCompletelyVisibleItem() ?: return
 
-        val viewType: Int = viewTypeMap[view.id] ?: recyclerView.adapter?.let { adapter ->
+        val firstSticker = stickerMap[first.id]
+        val nextSticker = stickerMap[next.id]
+        if (firstSticker == null && nextSticker == null) return
+
+        if (firstSticker != null) {
+            if (firstSticker.holder == null) {
+                val viewType = getViewType(listener, first) ?: return
+                firstSticker.viewType = viewType
+                firstSticker.holder?.itemView.replaceParent(this)
+            }
+
+            val hash = System.identityHashCode(first)
+            if (firstSticker.hash != hash) {
+                val holder = recyclerView.findContainingViewHolder(first) ?: return
+                refreshHolder(firstSticker.holder, holder.layoutPosition)
+                firstSticker.hash = hash
+            }
+        }
+
+        if (nextSticker != null) {
+            if (nextSticker.holder == null) {
+                val viewType = getViewType(listener, first) ?: return
+                nextSticker.viewType = viewType
+            }
+        }
+
+        stick(firstSticker, nextSticker, next)
+    }
+
+    private fun stick(pre: Sticker?, next: Sticker?, nextView: View) {
+        if (pre != null && next != null) {// 两个头部相邻，不需要粘性悬浮
+            pre.holder?.itemView.removeFromParent()
+            curSticker?.holder?.itemView.removeFromParent()
+            curSticker = null
+        } else if (pre != null && next == null && curSticker != pre) {
+            curSticker?.holder?.itemView.removeFromParent()
+            prepareHolder(pre)
+            pre.holder?.itemView.replaceParent(this)
+            curSticker = pre
+        } else if (pre == null && next != null && curSticker != null) {
+            curSticker?.holder?.itemView?.also {
+                it.top = nextView.top - it.height
+            }
+        }
+    }
+
+    private fun getViewType(listener: RecyclerView.OnScrollListener, child: View): Int? {
+        recyclerView.adapter?.let { adapter ->
             var pos = RecyclerView.NO_POSITION
-            view.layoutParams.am<RecyclerView.LayoutParams> {
+            child.layoutParams.am<RecyclerView.LayoutParams> {
                 pos = it.absoluteAdapterPosition
             }
 
             if (pos == RecyclerView.NO_POSITION) {
                 recyclerView.removeOnScrollListener(listener)
-                return
+                return null
             }
 
-            adapter.getItemViewType(pos)
-        } ?: return
+            return adapter.getItemViewType(pos)
+        }
+        return null
+    }
 
-        viewTypeMap[view.id] = viewType
-
-        if (holderMap[viewType] == null) {
+    private fun prepareHolder(sticker: Sticker) {
+        if (sticker.holder == null) {
             recyclerView.adapter?.also { adapter ->
-                val headHolder = adapter.createViewHolder(this@StickHeadViewX, viewType)
-                headHolder.itemView.replaceParent(this)
-                holderMap[viewType] = headHolder
+                val headHolder = adapter.createViewHolder(this@StickHeadViewX, sticker.viewType)
+                headHolder.setIsRecyclable(false)
+                sticker.holder = headHolder
             }
+        }
+    }
+
+    private fun refreshHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+        onNotNull(holder, recyclerView.adapter) { vh, adapter ->
+            adapter.onBindViewHolder(vh, position)
         }
     }
 
@@ -80,3 +140,5 @@ class StickHeadViewX @JvmOverloads constructor(
         return null
     }
 }
+
+data class Sticker(var viewType: Int = -1, var holder: RecyclerView.ViewHolder?, var hash: Int)
