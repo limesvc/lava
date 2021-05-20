597d1b4a1747f977b9ed5c3107081dbe7ff6a1c8
svc <wuxi@flatincbr.com>
2021-05-20 18:53:15 +0800

    增加map转换

---
+++ core/src/main/kotlin/lava/core/ext/Array.kt
@@ -0,0 +1,14 @@
+package lava.core.ext
+
+/**
+ * Created by svc on 2021/5/20
+ */
+
+fun <M, K, V> Array<M>.toMap(map: (M) -> Pair<K, V>): Map<K, V> {
+    val result = mutableMapOf<K, V>()
+    this.forEach {
+        val pair = map(it)
+        result[pair.first] = pair.second
+    }
+    return result
+}
